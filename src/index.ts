c59b0c4edb42c30bcca83e6ce49b8864b87e104a
sjx233 <sjx233@qq.com>
2019-01-23 15:16:55 +0800

    Clamp arguments to avoid parsing errors

--- src/index.ts
+++ src/index.ts
@@ -27,14 +27,18 @@ export enum SoundSource {
   VOICE = "voice"
 }
 
-export function playSound(sound: string | ResourceLocation, source: SoundSource, volume?: number, pitch?: number, minimumVolume?: number) {
-  let command = `execute as @a at @s run playsound ${ResourceLocation.from(sound)} ${source} @s ~ ~ ~`;
-  const pitchDefined = pitch !== undefined;
-  const minimumVolumeDefined = minimumVolume !== undefined;
-  if (volume !== undefined) command += " " + volume;
-  else if (pitchDefined || minimumVolumeDefined) command += " 1";
-  if (pitchDefined) command += " " + pitch;
-  else if (minimumVolumeDefined) command += " 1";
-  if (minimumVolumeDefined) command += " " + minimumVolume;
-  return command;
+export function playSound(sound: string | ResourceLocation, source: SoundSource, volume = 1, pitch = 1, minimumVolume = 0) {
+  const soundId = ResourceLocation.from(sound);
+  const baseCommand = `execute as @a at @s run playsound ${soundId} ${source} @s`;
+  volume = Math.max(volume, 0);
+  pitch = clamp(pitch, 0, 2);
+  minimumVolume = clamp(minimumVolume, 0, 1);
+  if (minimumVolume !== 0) return `${baseCommand} ~ ~ ~ ${volume} ${pitch} ${minimumVolume}`;
+  if (pitch !== 1) return `${baseCommand} ~ ~ ~ ${volume} ${pitch}`;
+  if (volume !== 1) return `${baseCommand} ~ ~ ~ ${volume}`;
+  return baseCommand;
+}
+
+function clamp(value: number, min: number, max: number) {
+  return value < min ? min : value > max ? max : value;
 }
