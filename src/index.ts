c5cacd0a451482626b51a371acb0817fc11436fd
sjx233 <sjx233@qq.com>
2019-01-23 15:38:56 +0800

    Use Math.clamp instead

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "ecma-proposal-math-extensions";
 import ResourceLocation from "resource-location";
 
 export enum Instrument {
@@ -31,14 +32,10 @@ export function playSound(sound: string | ResourceLocation, source: SoundSource,
   const soundId = ResourceLocation.from(sound);
   const baseCommand = `execute as @a at @s run playsound ${soundId} ${source} @s`;
   volume = Math.max(volume, 0);
-  pitch = clamp(pitch, 0, 2);
-  minimumVolume = clamp(minimumVolume, 0, 1);
+  pitch = Math.clamp(pitch, 0, 2);
+  minimumVolume = Math.clamp(minimumVolume, 0, 1);
   if (minimumVolume !== 0) return `${baseCommand} ~ ~ ~ ${volume} ${pitch} ${minimumVolume}`;
   if (pitch !== 1) return `${baseCommand} ~ ~ ~ ${volume} ${pitch}`;
   if (volume !== 1) return `${baseCommand} ~ ~ ~ ${volume}`;
   return baseCommand;
-}
-
-function clamp(value: number, min: number, max: number) {
-  return value < min ? min : value > max ? max : value;
 }
